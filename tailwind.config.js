86b754d6951297de661bff88c0e6b820ca280465
Rohan R Mallya <bangalorerohan@gmail.com>
2024-07-28 13:03:08 +0530

    home: making homepage responsive, prettifying, adding shared of ro-pink

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   content: [
-    './_drafts/**/*.html',
-    './_includes/**/*.html',
-    './_layouts/**/*.html',
-    './_posts/*.md',
-    './*.md',
-    './*.html',
+    "./_drafts/**/*.html",
+    "./_includes/**/*.html",
+    "./_layouts/**/*.html",
+    "./_posts/*.md",
+    "./*.md",
+    "./*.html",
   ],
   theme: {
     extend: {
       colors: {
-        'ro-blue': {
+        "ro-blue": {
           50: "#DADDFB",
           100: "#B5BCF7",
           200: "#6775EF",
@@ -21,15 +21,27 @@ module.exports = {
           700: "#050A33",
           800: "#030620",
           900: "#020412",
-          950: "#010209"
+          950: "#010209",
         },
 
-        'ro-nav': '#091153',
+        "ro-nav": "#091153",
 
-        "ro-pink": "#DA1881"
-      }
-
+        "ro-pink": {
+          DEFAULT: "#DA1881",
+          50: "#F7B3D8",
+          100: "#F5A1CE",
+          200: "#F17CBB",
+          300: "#ED57A8",
+          400: "#E83295",
+          500: "#DA1881",
+          600: "#A71263",
+          700: "#750D45",
+          800: "#420727",
+          900: "#100209",
+          950: "#000000",
+        },
+      },
     },
   },
-  plugins: []
-}
+  plugins: [],
+};
